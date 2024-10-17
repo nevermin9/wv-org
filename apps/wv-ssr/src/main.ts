a230179b018406af481169e73d330d5a44a67ee8
xanox <anton.chistik@acctopus.de>
2024-10-17 20:49:04 +0200

    feat: migrated to deno

--- apps/wv-ssr/src/main.ts
+++ apps/wv-ssr/src/main.ts
@@ -1,40 +1,40 @@
-import { createServer } from "node:http";
 import Mustache from "mustache";
 //import { resolve } from "node:path"
-
 import { createRoutes } from "./app/app.ts";
-
-
-const getName = () => {
-  const name = "AAAA".toUpperCase();
-  return name;
-};
-
 
 const __dirname = new URL(".", import.meta.url).pathname;
 console.log({ __dirname });
+console.log({ dirname: import.meta.dirname })
+
+console.log(import.meta)
 
 // const STATIC_PATHNAME = "/public";
 // const STATIC_DIR_PATH = resolve(__dirname, `.${STATIC_PATHNAME}`);
 // const NOT_FOUND_PAGE = resolve(STATIC_DIR_PATH, "./404.html");
 
-const server = createServer(async (req, res) => {
-  const router = await createRoutes({
-    db(model) {
-      return {
-        get(id: string) {
-          return { name: "Anton", age: 29 };
+Deno.serve(
+  { port: 3000 },
+  async (req) => {
+    const router = await createRoutes({
+      db(model) {
+        return {
+          get(id: string) {
+            return { name: "Anton", age: 29 };
+          }
         }
-      }
-    },
-    render: Mustache.render,
-  });
-  const url = new URL(`http://${ req.headers.host }`);
-  const pathname = url.pathname;
-  console.log({ method: req.method, path: pathname })
-  // console.log({ result: router.get(pathname) })
+      },
+      render: Mustache.render,
+    }) as Map<string, Controller>;
 
-  res.end("helllll");
-});
+    const url = new URL(req.url);
+    const pathname = url.pathname;
+    console.log({ method: req.method, path: pathname, url: req.url });
+    const controller = router.get(pathname);
 
-server.listen(3000);
+    if (controller) {
+      return new Response(controller.get({ url, params: { id: "20" } }));
+    }
+    return new Response("404");
+  }
+)
+
